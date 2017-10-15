426c657734b5a46a0513d87008b4e13cdcc4dff3
Zack Tylor <taylor.zackory@gmail.com>
2017-10-15 14:11:02 -0500

    use redux form to hande form actions, validations and submission.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
     return (
-      <div>
-        NEW POST
+      <div className={ className }>
+        <label>{ field.label }</label>
+        <input
+          className="form-control"
+          type="text"
+          { ...field.input }
+        />
+        <div className="text-help">
+          { touched ? error : '' }
+        </div>
       </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+  if (!values.title) {
+    errors.title = 'Title must not be left blank.';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Categories must not be left blank.';
+  }
+
+  if (!values.content) {
+    errors.content = 'Content must not be left blank.';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm',
+})(PostsNew);
